fe9dde4ed8e069322344520205258764538c571a
devpedropp <pedroppimentel3@gmail.com>
2024-12-11 17:52:59 -0300

    add regex with sed command

--- modulo05/aula01-vscode-terminal-regex/regex-terminal.sh
+++ modulo05/aula01-vscode-terminal-regex/regex-terminal.sh
@@ -4,3 +4,26 @@ find . -name *.test.js -not -path '*node_modules**'
 npm i -g ipt
 
 find . -name *.js -not -path '*node_modules**' | ipt
+
+# volta para a pasta do modulo05
+cp -r ../../modulo01/aula05-tdd-desafio-resolvido .
+
+# find . -name *.js -not -path '*node_modules**' \
+# | ipt -o \
+# | xargs -I '{file}' echo 'ae' {file}
+
+# 1s -> primeira linha
+# ^ -> primeira coluna
+# substitui pelo $CONTENT
+# quebrou a linha para adicionar um \n implícito
+CONTENT="'use strict';"
+find . -name *.js -not -path '*node_modules**' \
+| ipt -o \
+| xargs -I '{file}' sed -i "" -e '1s/^/\'$CONTENT'\
+/g' {file}
+
+# atualizar todos os arquivos sem precisar selecionar
+# CONTENT="'use strict';"
+# find . -name *.js -not -path '*node_modules**' \
+# | xargs -I '{file}' sed -i "" -e '1s/^/\'$CONTENT'\
+# /g' {file}
