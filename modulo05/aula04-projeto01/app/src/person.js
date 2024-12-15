85732404f153319da6021ec88e70da10d00116f3
devpedropp <pedroppimentel3@gmail.com>
2024-12-15 10:26:18 -0300

    add aula04 parte1

---
+++ modulo05/aula04-projeto01/app/src/person.js
@@ -0,0 +1,47 @@
+const { evaluateRegex } = require('./util');
+
+class Person {
+    // replace => (\w+):\s.*
+    // por => $1,
+    constructor([
+        nome,
+        nacionalidade,
+        estadoCivil,
+        documento,
+        rua,
+        numero,
+        bairro,
+        estado,
+    ]) {
+        // ^ -> começo da string
+        // + -> uma ou mais ocorrẽncias
+        // (\w{1}) -> pega só a primeira letra e deixa em um grupo
+        // (a-zA-Z)+$ -> encontra letras maiúsculas e minúsculas, adicionamos o + para pegar todas até o caracter especial
+        // /g -> todas as ocorrências que encontrar
+        const firstLetterExp = evaluateRegex(/^(\w{1})([a-zA-Z]+$)/g);
+        const formatFirstLetter = (prop) => {
+            return prop.replace(firstLetterExp, (fullMatch, group1, group2, index) => {
+                return `${group1.toUpperCase()}${group2.toLowerCase()}`;
+            })
+        }
+
+        // replace => (\w+),
+        // por => this.$1 = $1
+        this.nome = nome;
+        this.nacionalidade = formatFirstLetter(nacionalidade);
+        this.estadoCivil = formatFirstLetter(estadoCivil);
+        // tudo que NÃO for dígito vira vazio
+        // /g serve para remover todas as ocorrências que encontrar
+        this.documento = documento.replace(evaluateRegex(/\D/g), '');
+        // começa a procurar depois do " a " e pega tudo que estiver a frente
+        // ?<= faz com que ignore tudo que estiver antes desse match, positive lookBehind
+        this.rua = rua.match(evaluateRegex(/(?<=\sa\s).*$/)).join();
+        this.numero = numero;
+        // começa a buscar depois do espaço, pega qualquer letra ou dígito até o final da linha (poderia ser o .* tbm)
+        this.bairro = bairro.match(evaluateRegex(/(?<=\s)\w+$/)).join();
+        // remove o ponto literal (.) do final da frase
+        this.estado = estado.replace(evaluateRegex(/\.$/), '');
+    }
+}
+
+module.exports = Person;
---
+++ modulo05/aula04-projeto01/app/src/textProcessorFluentAPI.js
@@ -0,0 +1,63 @@
+const { evaluateRegex } = require('./util');
+
+// o objetivo do Fluent API é executar tarefas como um pipeline, step by step
+// e no fim, chamar o build. Muito similar ao padrão Builder,
+// a diferença que aqui é sobre processos, o Builder é sobre construção de objetos
+class TextProcessorFluentAPI {
+    // propriedade privada
+    #content;
+    constructor(content) {
+        this.#content = content;
+    }
+
+    extractPeopleData() {
+        // ?<= fala que vai extrair os dados que virão depois desse grupo
+        // [contratante|contratada] ou um ou outro (com a flag i, case insensitive, no fim da expressão regular pra pegar maiúsculas e minúsculas)
+        // :\s{1} vai procurar o caracter literal de dois pontos seguindo de um espaço
+        // tudo acima fica dentro de um parênteses significando que vai pegar daí pra frente
+
+        // (?!\s) negative look around, vai ignorar os contratantes do fim do documento (que só tem espaço na frente deles)
+        // .*\n pega qualquer coisa até o primeiro \n
+        // .*? non greety, esse ? faz com que pare na primeira ocorrência, evitando loops
+
+        // $ informa que a pesquisa acaba no fim da linha
+        // g -> global
+        // m -> multiline
+        // i -> case insensitive
+
+        const matchPerson = evaluateRegex(/(?<=[contratante|contratada]:\s{1})(?!\s)(.*\n.*?)$/gmi);
+
+        // faz o match para encontrar a string inteira que contém os dados que precisamos
+        const onlyPerson = this.#content.match(matchPerson);
+
+        // console.log('onlyPerson', onlyPerson);
+        // console.log('onlyPerson', matchPerson.test(this.#content));
+
+        this.#content = onlyPerson;
+        return this;
+    }
+
+    dividedTextInColumns() {
+        const splitRegex = evaluateRegex(/,/);
+        this.#content = this.#content.map(line => line.split(splitRegex));
+
+        return this;
+    }
+
+    removeEmptyCharacters() {
+        const trimSpaces = evaluateRegex(/^\s+|\s+$|\n/g);
+        this.#content = this.#content.map(line => line.map(item => item.replace(trimSpaces, '')));
+
+        return this;
+    }
+
+    mapPerson() {
+        return this;
+    }
+
+    build() {
+        return this.#content;
+    }
+}
+
+module.exports = TextProcessorFluentAPI;
