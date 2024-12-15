d2508ccdaf8160c03edbd44be2621736a61f92ae
devpedropp <pedroppimentel3@gmail.com>
2024-12-15 09:07:57 -0300

    add aula03 projeto01 with safe regex

---
+++ modulo05/aula03-projeto01/app/test/util.test.js
@@ -0,0 +1,25 @@
+const { describe, it } = require('mocha');
+const { expect } = require('chai');
+const { evaluateRegex, InvalidRegexError } = require('./../src/util');
+
+describe('Util', () => {
+    it('#evaluateRegex should throw an error using an unsafe regex', () => {
+        const unsafeRegex = /^([a-zA-Z0-9]+\s?)+$/;
+
+        /* 
+        CUIDADO AO RODAR! fica rodando em loop infinitamente, catastrophic backtracking 
+        time \
+        node --eval "/^([a-zA-Z0-9]+\s?)+$/.test('eae man como vai voce e como vai voce e como vai voce?') && console.log('legalzin')"
+        */
+
+        expect(() => evaluateRegex(unsafeRegex)).to.throw(InvalidRegexError, `This ${unsafeRegex} is unsafe dude!`);
+    });
+
+    it('#evaluateRegex should not throw an error using a safe regex', () => {
+        const safeRegex = /^([a-z])$/;
+        expect(() => evaluateRegex(safeRegex)).to.not.throw;
+        expect(evaluateRegex(safeRegex)).to.be.ok;
+    })
+});
+
+
